65743cd4033615dd7580b9026e0b053674b0c46f
Alumno <ulsa@iMac-de-Alumno.local>
2018-09-20 13:39:52 -0700

    first commit

---
+++ NavegacionTableView/ViewControllers/DetalleMateriaController.swift
@@ -0,0 +1,59 @@
+//
+//  DetalleMateriaController.swift
+//  NavegacionTableView
+//
+//  Created by Alumno on 20/09/18.
+//  Copyright © 2018 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class   DetalleMateriaController : UIViewController {
+    
+    
+    
+    @IBOutlet weak var lblCalificacionPrimerParcial: UILabel!
+    
+    @IBOutlet weak var lblCalificacionSegundoParcial: UILabel!
+    
+    @IBOutlet weak var lblCalificacionTercerParcial: UILabel!
+    
+    @IBOutlet weak var lblCalificacionFinal: UILabel!
+    
+    
+    var materia : Materia?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let materiaActual = materia {
+            self.title = materiaActual.nombre
+            
+            if let primerParcial = materiaActual.calificacionPrimerParcial {
+                lblCalificacionPrimerParcial.text = "\(primerParcial)"
+            }
+            else{
+                lblCalificacionPrimerParcial.text = "..."
+            }
+            if let segundoParcial = materiaActual.calificacionSegundoParcial {
+                lblCalificacionSegundoParcial.text = "\(segundoParcial)"
+            }
+            else{
+                lblCalificacionSegundoParcial.text = "..."
+            }
+            if let tercerParcial = materiaActual.calificacionTercerParcial {
+                lblCalificacionPrimerParcial.text = "\(tercerParcial)"
+            }
+            else{
+                lblCalificacionTercerParcial.text = "..."
+            }
+            if let final = materiaActual.califcacionFinal {
+                lblCalificacionFinal.text = "\(final)"
+            }
+            else{
+                lblCalificacionTercerParcial.text = "..."
+            }
+        }
+    }
+}
